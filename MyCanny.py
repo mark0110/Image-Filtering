478f24052dff7f6093cab7209ff15d7423c2a5e7
Mark <mark.volfson@gmail.com>
2019-02-17 12:34:05 -0500

    h

--- MyCanny.py
+++ MyCanny.py
@@ -5,7 +5,20 @@ import time
 import scipy.signal
 
 def Mycanny(sigma):
-    xKernal = cv.getGaussianKernel(ksize=5, sigma=sigma)
+    xKernal = cv.getGaussianKernel(ksize=5, sigma=2)
     yKernal = np.transpose(xKernal)
 
-    img = cv.imread()
+    img = cv.imread("bowl-of-fruit.jpg", cv.IMREAD_GRAYSCALE)
+
+    conImg = scipy.signal.convolve(img, xKernal)
+    conImg = scipy.signal.convolve(conImg, yKernal)
+
+    lopImg = cv.Laplacian(conImg, cv.CV_16S, 3)
+    lopImgFinal = cv.convertScaleAbs(lopImg)
+
+    cv.imshow("lop", lopImgFinal)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+Mycanny(3)
